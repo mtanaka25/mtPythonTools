7410c8c06103de710f2abb4ec13004bb915bb09b
TanaPanda <masaki.tanaka@tnpnd.com>
2022-10-17 04:15:26 -0500

    make setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+packages = [
+    'mtPyEcon',
+    'mtPyTools'
+]
+
+setup(
+    name='mtPythonTools',
+    description="Miscellaneous Python tools for Economic Resaech",
+    long_description="Masaki Tanaka's miscellaneous Python tools for Economic Resaech",
+    url='https://masaki-tanaka.com',
+    author='Masaki Tanaka',
+    author_email='masaki.tanaka@wustl.edu',
+    license='GPL-3.0 license',
+    version='0.1.0',
+    packages=packages,
+)
